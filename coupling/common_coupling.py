4435e193ff169ab9ad194d81bdfd3b7570c61d49
dzon4xx <janpleszynski@gmail.com>
2022-01-31 08:51:22 +0100

    Improve common coupling example.

--- coupling/common_coupling.py
+++ coupling/common_coupling.py
@@ -1,18 +1,29 @@
-counter = 1
-
-
 class A:
-    def increment_counter(self):
-        global counter
-        counter += 1
+    def __init__(self, data: dict):
+        self._data = data
+
+    def set_config(self):
+        self._data["a"] = "abcd"
+
+    def get_len(self):
+        return len(self._data["a"])
 
 
 class B:
-    def increment_counter(self):
-        global counter
-        counter += 1
+    def __init__(self, data: dict):
+        self._data = data
+
+    def set_config(self):
+        self._data["a"] = 1
 
 
 def main():
-    A().increment_counter()
-    B().increment_counter()
+    config = {"a": 2}
+    a = A(config)
+    b = B(config)
+
+    a.set_config()
+    a.get_len()
+    b.set_config()
+    a.get_len()
+
